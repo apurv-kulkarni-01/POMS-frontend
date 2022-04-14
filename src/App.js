c79ca1cad11a6b24c48e5ce1ee0c4a7b005eb040
Apurv <apurvkulkarni29@gmail.com>
2022-04-14 19:54:39 +0530

    initial trials

--- src/App.js
+++ src/App.js
@@ -1,10 +1,3 @@
-// import { IconButton } from "@chakra-ui/button";
-// import { useColorMode } from "@chakra-ui/color-mode";
-// import { Flex, VStack, Heading, Spacer } from "@chakra-ui/layout";
-// import { FaSun, FaMoon } from "react-icons/fa";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import { Flex, VStack, Heading, Switch } from "@chakra-ui/react";
-
 import Home from "./pages/Home"
 import React from "react";
 
