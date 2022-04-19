51911202d1355d5e246e2edc998650657a307f2c
Shrenik <shrenikparam1234@gmail.com>
2022-04-19 23:59:08 +0530

    Add UserSetting Page

---
+++ src/pages/UserSettings.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import HeaderC from "../components/HeaderC";
+import {ChakraProvider, Box, HStack, VStack, FormControl, FormLabel, Input, Button} from '@chakra-ui/react';
+import Typography from "@mui/material/Typography";
+import theme from '../theme/index'
+
+const Usersettings = () => {
+  return (
+    <>
+    <HeaderC />
+    <center>
+    <Typography
+        // fontFamily="Cursive"
+        fontSize="28px"
+        // variant="h6"
+        fontWeight="normal"
+    >
+    Edit user settings
+    </Typography><br/><br/><br/>
+    <Box w='33%'>
+    <HStack spacing='24px'>
+        <Typography
+            // fontFamily="Cursive"
+            fontSize="24px"
+            // variant="h4"
+            fontWeight="semibold"
+        >
+        Notifications
+        </Typography>
+        <VStack spacing='24px'>
+            <Typography
+                // fontFamily="Cursive"
+                fontSize="16px"
+                align='left'
+            >
+            We require your email to send you product<br/> and account related updates.
+            </Typography><br/>
+                <ChakraProvider theme={theme}>
+            <form>
+                <FormControl>
+                    <FormLabel fontWeight='semibold' fontSize='16px' >Email</FormLabel>
+                    <Input type="email" placeholder="Email" />
+                </FormControl><br/>
+                <Button style={{float: 'left'}} type='submit' variant='solid' colorScheme='accept' >
+                    Save changes
+                </Button>
+            </form>
+                </ChakraProvider>
+        </VStack>
+    </HStack>
+    </Box>
+    </center>
+    </>  
+  );
+};
+
+export default Usersettings
